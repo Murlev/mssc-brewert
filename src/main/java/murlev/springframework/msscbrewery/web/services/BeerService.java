9667ce16d5aac34e73225aef2edb1bbed3b0a675
Metodi Murlev <methody.myrlev@duosoft.net>
2023-11-20 17:15:27 +0200

    return beer by id (hardcoded data)

---
+++ src/main/java/murlev/springframework/msscbrewery/web/services/BeerService.java
@@ -0,0 +1,9 @@
+package murlev.springframework.msscbrewery.web.services;
+
+import murlev.springframework.msscbrewery.web.model.BeerDto;
+
+import java.util.UUID;
+
+public interface BeerService {
+    BeerDto getBeerById(UUID beerId);
+}
---
+++ src/main/java/murlev/springframework/msscbrewery/web/services/BeerServiceImpl.java
@@ -0,0 +1,13 @@
+package murlev.springframework.msscbrewery.web.services;
+
+import murlev.springframework.msscbrewery.web.model.BeerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+@Service
+public class BeerServiceImpl implements BeerService {
+    @Override
+    public BeerDto getBeerById(UUID beerId) {
+        return BeerDto.builder().id(UUID.randomUUID()).beerName("Galaxy cat").beerStyle("Pale Ale").build();
+    }
+}
